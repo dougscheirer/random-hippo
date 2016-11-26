ae88c21f98685a6a431472ff3ba4fb01929cf77f
Doug Scheirer <dscheirer@yahoo.com>
2016-11-26 23:50:39 +0000

    init random hippo

---
+++ usr/local/bin/button-or-sleep.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from gpiozero import LED, Button
+from signal import pause
+from time import sleep
+import sys
+
+def flash(led, interval, count):
+	for i in range(0, count):
+		led.off()
+		sleep(interval)
+		led.on()
+		sleep(0.01)
+	led.off()
+	return
+
+wait_time = sys.argv[1]
+
+led=LED(18)
+button=Button(23)
+
+flash(led, 0.5, 4)
+retval=button.wait_for_press(int(wait_time))
+led.on()
+print(retval)
+led.close()
+
