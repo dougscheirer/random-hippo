df10fdc0c8f02c63a48d9b259a45d59db4a7ae28
Doug Scheirer <doug@dscheirer.com>
2018-12-26 16:24:18 -0800

    adding a wifi config script for USB wifi on older models

---
+++ wifi-usb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ] ; then
+	echo "Must run as root" && exit 1
+fi
+
+SSID=$1
+PASS=$2
+
+if [ "$SSID" == "" ] || [ "$PASS" == "" ] ; then
+	echo "need SSID and password" && exit 1
+fi
+
+EXISTS=$(grep $SSID /etc/wpa_supplicant/wpa_supplicant.conf)
+if [ ! "$EXISTS" == "" ] ; then
+	echo "$SSID is already configured in /etc/wpa_supplicant/wpa_supplicant.conf" && exit 1
+fi
+
+NETBLOCK=$(wpa_passphrase $SSID $PASS | sed 's/#psk=.*//g')
+cat <<EOF > /etc/wpa_supplicant/wpa_supplicant.conf
+country=US
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+
+$NETBLOCK
+EOF
+
+# reconfigure
+wpa_cli reconfigure
